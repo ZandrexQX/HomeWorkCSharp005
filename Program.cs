cd8bf20f7350d94cc9f6d1a93cc81bed1ab16a3e
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-02-11 11:17:39 +0300

    Task 34

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Task 34");
+Console.WriteLine("------");
+
+void InputArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 1000);
+}
+
+
+int ReleaseArray(int[] array)
+{
+  int count = 0;
+  foreach (int item in array)
+  {
+    if (item%2 == 0) count++;
+  }
+  return count;
+}
+
+int[] array = new int[10];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Количество четных элементов: {ReleaseArray(array)}");
+
+Console.ReadLine();
+
+/*--------------------------------------------*/
