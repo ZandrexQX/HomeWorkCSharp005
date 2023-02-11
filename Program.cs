5bc364b54736bc8a998eabe102385a9fcdd60fcf
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-02-11 11:24:42 +0300

    Task 36

--- Program.cs
+++ Program.cs
@@ -1,28 +1,58 @@
-﻿Console.Clear();
-Console.WriteLine("Task 34");
+﻿// Console.Clear();
+// Console.WriteLine("Task 34");
+// Console.WriteLine("------");
+
+// void InputArray1(int[] array)
+// {
+//   for (int i = 0; i < array.Length; i++)
+//     array[i] = new Random().Next(100, 1000);
+// }
+
+
+// int ReleaseArray1(int[] array)
+// {
+//   int count = 0;
+//   foreach (int item in array)
+//   {
+//     if (item%2 == 0) count++;
+//   }
+//   return count;
+// }
+
+// int[] array1 = new int[10];
+// InputArray1(array1);
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array1)}]");
+// Console.WriteLine($"Количество четных элементов: {ReleaseArray1(array1)}");
+
+// Console.ReadLine();
+
+// /*--------------------------------------------*/
+
+Console.Clear();
+Console.WriteLine("Task 36");
 Console.WriteLine("------");
 
-void InputArray(int[] array)
+void InputArray2(int[] array)
 {
   for (int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(100, 1000);
+    array[i] = new Random().Next(-10, 10);
 }
 
 
-int ReleaseArray(int[] array)
+int ReleaseArray2(int[] array)
 {
-  int count = 0;
-  foreach (int item in array)
+  int sum = 0;
+  for (int i = 1; i < array.Length; i+=2)
   {
-    if (item%2 == 0) count++;
+    sum += array[i];
   }
-  return count;
+  return sum;
 }
 
-int[] array = new int[10];
-InputArray(array);
-Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
-Console.WriteLine($"Количество четных элементов: {ReleaseArray(array)}");
+int[] array2 = new int[8];
+InputArray2(array2);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array2)}]");
+Console.WriteLine($"Сумма элементов на нечетных позициях: {ReleaseArray2(array2)}");
 
 Console.ReadLine();
 
