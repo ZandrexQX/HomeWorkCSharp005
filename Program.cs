4556aa7846fd6418a2cce77cc41924748153bea3
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-02-11 14:40:05 +0300

    Task 38

--- Program.cs
+++ Program.cs
@@ -28,31 +28,64 @@
 
 // /*--------------------------------------------*/
 
+// Console.Clear();
+// Console.WriteLine("Task 36");
+// Console.WriteLine("------");
+
+// void InputArray2(int[] array)
+// {
+//   for (int i = 0; i < array.Length; i++)
+//     array[i] = new Random().Next(-10, 10);
+// }
+
+
+// int ReleaseArray2(int[] array)
+// {
+//   int sum = 0;
+//   for (int i = 1; i < array.Length; i+=2)
+//   {
+//     sum += array[i];
+//   }
+//   return sum;
+// }
+
+// int[] array2 = new int[8];
+// InputArray2(array2);
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array2)}]");
+// Console.WriteLine($"Сумма элементов на нечетных позициях: {ReleaseArray2(array2)}");
+
+// Console.ReadLine();
+
+// /*--------------------------------------------*/
+
 Console.Clear();
-Console.WriteLine("Task 36");
+Console.WriteLine("Task 38");
 Console.WriteLine("------");
 
-void InputArray2(int[] array)
+void InputArray3(double[] array)
 {
   for (int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(-10, 10);
+    array[i] = Math.Round(new Random().NextDouble()*100, 2);
 }
 
 
-int ReleaseArray2(int[] array)
+double ReleaseArray3(double[] array)
 {
-  int sum = 0;
-  for (int i = 1; i < array.Length; i+=2)
-  {
-    sum += array[i];
-  }
-  return sum;
+    double max = 0, min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (min > array[i]) min = array[i];
+        if (max < array[i]) max = array[i];
+    }
+    Console.WriteLine(min);
+    Console.WriteLine(max);
+    return Math.Round(max - min,2);
 }
 
-int[] array2 = new int[8];
-InputArray2(array2);
-Console.WriteLine($"Начальный массив: [{string.Join(", ", array2)}]");
-Console.WriteLine($"Сумма элементов на нечетных позициях: {ReleaseArray2(array2)}");
+double[] array3 = new double[8];
+InputArray3(array3);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array3)}]");
+Console.WriteLine($"Разница между минимальным и максимальным значениями: {ReleaseArray3(array3)}");
 
 Console.ReadLine();
 
